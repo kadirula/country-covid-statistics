73652c4e856695b73a7f11579ae0a42cacc4b1d3
kadirula <kadirula17@gmail.com>
2022-10-14 19:43:43 +0300

    Header Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // import Header from "./components/Header";
-import Header from "./components/Header/Header";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import CountryStatisticDetail from "./pages/CountryStatisticDetail";
